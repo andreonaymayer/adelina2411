24a02aa3d779e58cf2025a527d6de41db1ed65c3
bryan mann cunha <bryanmanncunha@gmail.com>
2022-11-23 09:19:11 -0300

    commit parcial

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 VALORMINIMOPARA_XYZW = 0
 VALORMAXIMOPARA_XYZW = 10
 QUANTIDADE_POPULACAO = 10
-
+QUANTIDADE_MUTACAO = 5
+CORTE = 4
 
 # for n in range(0,11):
 #     numero = format(n,'b')
@@ -40,6 +41,22 @@ def toBin(n):
         return '1001'
     elif n == 10:
         return '1010'
+def trocaBinToInt(nomeBin):
+    decimal =0
+    binario = int(nomeBin)
+    i=0
+    n=len(str(nomeBin))
+    while n>=0:
+        resto = binario % 10
+        decimal =decimal +(resto * (2**i))
+        n=n-1
+        i=i+1
+        binario=binario//10
+    decimal = str(decimal)
+    while decimal.__len__() < 4:
+        decimal = '0'+decimal
+    return int(decimal)
+
 
 
 def funcaoFitness(x, y, z, w):
@@ -72,26 +89,92 @@ def cria_populacao_inicial(numero=QUANTIDADE_POPULACAO):
 
 def corte(num,lista):
     return lista[:num]
-
-populacaoInicial = cria_populacao_inicial()
-
-melhores = corte(4,populacaoInicial)
-
-for um in melhores:
-
-
-
-def criaNovaPopulacao(_nova, _corte):
+def setDict(x=None, y=None,z=None, w=None, bin=None, fit=None):
+    return {'x': x, 'y': y,'z':z ,'w':w,'bin': bin, 'fit': fit}
+def criaNovaPopulacao(_nova, _corte) :
     c = 0
     t = 4
     for cut in _corte:
+        print(cut)
         while c < t:
             _nova.append(setDict(bin='{}{}'.format(
-                cut[2][:-2],
-                _corte[random.randint(0, _corte.__len__() - 1)][2][4:]
+                cut[4][:-5],
+                _corte[random.randint(0, _corte.__len__() - 1)][4][cut[4].__len__()-(CORTE+1):]
             )))
             t -= 1
         c += 1
-        t = 4;
+        t = 4
     return _nova
+def trocaCaractere(nome: str, posicao, novoCaractere):
+    retorno = ''
+    c = 0
+    while c < nome.__len__():
+        if c == posicao:
+            retorno = retorno + novoCaractere
+        else:
+            retorno = retorno + nome[c]
+        c += 1
+    return retorno
+
+def mutacao(__nova):
+    historico = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for contador in range(0, QUANTIDADE_MUTACAO):
+        retirar = random.randint(0, historico.__len__() - 1)
+        numeroAleatorioDaLista = historico[retirar]
+        historico.remove(numeroAleatorioDaLista)
+        binarioStr = __nova[numeroAleatorioDaLista].get('bin')
+        posicaoDoGene = random.randint(0, binarioStr.__len__() - 1)
+        geneQueSeraModificado = binarioStr[posicaoDoGene]
+
+        if geneQueSeraModificado == '1':
+            binarioStr = trocaCaractere(binarioStr, posicaoDoGene, '0')
+        else:
+            binarioStr = trocaCaractere(binarioStr, posicaoDoGene, '1')
+
+        __nova[numeroAleatorioDaLista].update(setDict(bin=binarioStr))
+    return __nova
+def converteCromossomo(___novaPopulacao):
+    for item in ___novaPopulacao:
+        cromossomo = item.get('bin')
+        x = cromossomo[:4]
+        y =  cromossomo[4:8]
+        z = cromossomo[8:12]
+        w = cromossomo[12:16]
+        r = [trocaBinToInt(x),trocaBinToInt(y),trocaBinToInt(z),trocaBinToInt(w)]
+        item.update(setDict(r[0], r[1], r[2], r[3],cromossomo,funcaoFitness(r[0], r[1], r[2], r[3])))
+    return ___novaPopulacao
+def dictToList(dic: dict):
+    mat = []
+    for d in dic:
+        mat.append([d.get('x'), d.get('y'),d.get('z'), d.get('w'), d.get('bin'), d.get('fit')])
+    return mat
+
+def verificaFim(matr):
+    print(matr[0][5])
+    if matr[0][5] == 185:
+      return True
+    return False
+
+populacaoInicial = cria_populacao_inicial()
+printer(populacaoInicial)
+matriz = populacaoInicial
+novaPopulacao = []
+
+while verificaFim(matriz) is False:
+
+    melhores = corte(CORTE,populacaoInicial)
+    criaNovaPopulacao(novaPopulacao,melhores)
+    printer(novaPopulacao)
+    mutacao(novaPopulacao)
+    print('')
+    printer(novaPopulacao)
+    converteCromossomo(novaPopulacao)
+    printer(novaPopulacao)
+    matriz = dictToList(novaPopulacao)
+    novaPopulacao = []
+    matriz=sorted(matriz, key=lambda w: w[5], reverse=True)
+
+    printer(matriz)
+
+
 
