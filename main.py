fc84940880b4691e9cb879a226888d055f5cb792
bryan mann cunha <bryanmanncunha@gmail.com>
2022-11-24 18:27:49 -0300

    sistema funcional

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import random
 import time
-
+TAMANHO_CROMOSSOMO = 3
 VALORMINIMOPARA_XYZW = 0
-VALORMAXIMOPARA_XYZW = 10
-VALORMAXIMOPARA_X = 15
-VALORMAXIMOPARA_Y = 14
-VALORMAXIMOPARA_Z = 5
+VALORMAXIMOPARA_XYZW = 7
+VALORMAXIMOPARA_X = 7
+VALORMAXIMOPARA_Y = 7
+VALORMAXIMOPARA_Z = 7
 QUANTIDADE_POPULACAO = 10
 QUANTIDADE_MUTACAO = 5
 CORTE = 4
+CRUZAMENTO = 4
 
-# for n in range(0,11):
-#     numero = format(n,'b')
-#     while numero.__len__() < 4:
-#         numero = '0'+numero
-#     print('if n == {}:\n\treturn \'{}\'\nel'.format(n,numero),end='')
 
 def printer(l: list):
     for x in l:
@@ -23,7 +19,7 @@ def printer(l: list):
 
 def toBin(n):
     numero = format(n, 'b')
-    while numero.__len__() < 4:
+    while numero.__len__() < TAMANHO_CROMOSSOMO:
         numero = '0'+numero
     return numero
 
@@ -39,14 +35,14 @@ def trocaBinToInt(nomeBin):
         i=i+1
         binario=binario//10
     decimal = str(decimal)
-    while decimal.__len__() < 4:
+    while decimal.__len__() < 3:
         decimal = '0'+decimal
     return int(decimal)
 
 
 
 def funcaoFitness(x, y, z, w):
-    return 5 * x + y * 2 + w + z ** 3
+    return ((5 * x) + (y ** 2)) + (w + (z ** 3))
 
 
 def rnd(valorMaximo =VALORMAXIMOPARA_XYZW):
@@ -59,7 +55,7 @@ def cria():
     w = rnd()
     cc = '{}{}{}{}'.format(toBin(x), toBin(y), toBin(z), toBin(w))
     ft = funcaoFitness(x, y, z, w)
-    return [x, y, z, w, cc, ft]
+    return [x, y, z, w, cc, ft, 0]
 
 
 def cria_populacao_inicial(numero=QUANTIDADE_POPULACAO):
@@ -70,11 +66,17 @@ def cria_populacao_inicial(numero=QUANTIDADE_POPULACAO):
     f = open('populacaoInicial.txt', 'w')
     f.write(lista.__str__())
     f.close()
-    return sorted(lista, key=lambda w: w[5], reverse=True)
+    return ordena(lista)
 
 
-def corte(num,lista):
+def corte(num ,lista):
     return lista[:num]
+def ordena(lista):
+    for valor in lista:
+        valor[6] = abs(185 - valor[5])
+    return sorted(lista, key=lambda w: w[6], reverse=False)
+
+
 def setDict(x=None, y=None,z=None, w=None, bin=None, fit=None):
     return {'x': x, 'y': y,'z':z ,'w':w,'bin': bin, 'fit': fit}
 def criaNovaPopulacao(_nova, _corte) :
@@ -82,9 +84,10 @@ def criaNovaPopulacao(_nova, _corte) :
     t = 4
     for cut in _corte:
         while c < t:
+            aleatorio=random.randrange(1, 12)
             _nova.append(setDict(bin='{}{}'.format(
-                cut[4][:-5],
-                _corte[random.randint(0, _corte.__len__() - 1)][4][cut[4].__len__()-(CORTE+1):]
+                cut[4][:aleatorio],
+                _corte[random.randint(0, _corte.__len__() - 1)][4][aleatorio:]
             )))
             t -= 1
         c += 1
@@ -105,9 +108,9 @@ def mutacao(__nova):
     historico = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for contador in range(0, QUANTIDADE_MUTACAO):
         retirar = random.randint(0, historico.__len__() - 1)
-        numeroAleatorioDaLista = historico[retirar]
-        historico.remove(numeroAleatorioDaLista)
-        binarioStr = __nova[numeroAleatorioDaLista].get('bin')
+        #numeroAleatorioDaLista = historico[retirar]
+        #historico.remove(numeroAleatorioDaLista)
+        binarioStr = __nova[retirar].get('bin')
         posicaoDoGene = random.randint(0, binarioStr.__len__() - 1)
         geneQueSeraModificado = binarioStr[posicaoDoGene]
 
@@ -116,22 +119,22 @@ def mutacao(__nova):
         else:
             binarioStr = trocaCaractere(binarioStr, posicaoDoGene, '1')
 
-        __nova[numeroAleatorioDaLista].update(setDict(bin=binarioStr))
+        __nova[retirar].update(setDict(bin=binarioStr))
     return __nova
 def converteCromossomo(___novaPopulacao):
     for item in ___novaPopulacao:
         cromossomo = item.get('bin')
-        x = cromossomo[:4]
-        y =  cromossomo[4:8]
-        z = cromossomo[8:12]
-        w = cromossomo[12:16]
+        x = cromossomo[:3]
+        y =  cromossomo[3:6]
+        z = cromossomo[6:9]
+        w = cromossomo[9:12]
         r = [trocaBinToInt(x),trocaBinToInt(y),trocaBinToInt(z),trocaBinToInt(w)]
         item.update(setDict(r[0], r[1], r[2], r[3],cromossomo,funcaoFitness(r[0], r[1], r[2], r[3])))
     return ___novaPopulacao
 def dictToList(dic: dict):
     mat = []
     for d in dic:
-        mat.append([d.get('x'), d.get('y'),d.get('z'), d.get('w'), d.get('bin'), d.get('fit')])
+        mat.append([d.get('x'), d.get('y'),d.get('z'), d.get('w'), d.get('bin'), d.get('fit'),0])
     return mat
 
 def verificaFim(matr):
@@ -146,21 +149,20 @@ print('População inicial')
 printer(populacaoInicial)
 matriz = populacaoInicial
 novaPopulacao = []
-geracao =0
+geracao = 0
 while verificaFim(matriz) is False:
-    geracao +=1
+    geracao += 1
     print('GERAÇÂO: ' + str(geracao))
     melhores = corte(CORTE,populacaoInicial)
     criaNovaPopulacao(novaPopulacao,melhores)
     print('Nova população')
     printer(novaPopulacao)
     mutacao(novaPopulacao)
-    printer(novaPopulacao)
     converteCromossomo(novaPopulacao)
-#    printer(novaPopulacao)
+    #printer(novaPopulacao)
     matriz = dictToList(novaPopulacao)
     novaPopulacao = []
-    matriz=sorted(matriz, key=lambda w: w[5], reverse=True)
+    matriz=ordena(matriz)
     print('Mutação')
     printer(matriz)
 
